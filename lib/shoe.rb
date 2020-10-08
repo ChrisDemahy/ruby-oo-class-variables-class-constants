8b05dc9e7d7fdfe3e0b55d356a1e590374b5b9a5
ChrisDemahy <demahyc@gmail.com>
2020-10-08 16:51:30 -0500

    Finshed book and shoe

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
-    @brand = brand
+    self.brand = brand
   end
 
   def cobble
@@ -11,4 +14,10 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brand=(brand)
+    # binding.pry
+    @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
+  end
 end
